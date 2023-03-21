99a8b57b2a2d4c489419d6e450c3fffd0236f54d
anujkumar09062001 <72484813+anujkumar09062001@users.noreply.github.com>
2023-03-21 19:14:30 +0530

    testimonials.

---
+++ src/app/components/TestimonialCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const TestimonialCard = ({ header, image, title, subTitle }) => {
+  return (
+    <div className="th-bg-white p-lg-5 p-4">
+      <div className="fw-semibold th-24">{header}</div>
+      <div className="row mt-3">
+        <div
+          className="col-lg-5 col-8 d-flex justify-content-start 
+              align-items-center"
+        >
+          <div className="">
+            <img
+              src={image}
+              alt="Harry"
+              width="80px"
+              height="80px"
+              className="th-br-40"
+            />
+          </div>
+          <div className="ms-2">
+            <div className="fw-bold">{title}</div>
+            <div className="">{subTitle}</div>
+          </div>
+        </div>
+        <div className="col-lg-7 col-4 d-flex align-items-center flex-row-reverse mt-2">
+          <i className="fas fa-quote-right th-40 th-primary"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
--- src/app/screens/Testimonials.js
+++ src/app/screens/Testimonials.js
@@ -1,17 +1,19 @@
 import React from "react";
-import BgCar from "../assets/img/car-bg-1.png";
+import Harry from "../assets/img/harry.jpg";
+import Ron from "../assets/img/ron.jpg";
+import TestimonialCard from "../components/TestimonialCard";
 import BookingContact from "./BookingContact";
 import Welcome from "./Welcome";
 
 const Testimonials = () => {
   return (
-    <div id="Testimonials">
+    <div id="Testimonials" className="th-bg-grey">
       <Welcome title="Testimonials" />
 
       <div className="container">
         <div className="row my-5">
           <div className="col-lg-2"></div>
-          <div className="col-lg-8 text-center p-lg-5 p-3 d-flex flex-column justify-content-center align-items-center">
+          <div className="col-lg-8 mt-lg-0 mt-5 text-center p-lg-5 py-4 px-3 d-flex flex-column justify-content-center align-items-center">
             <div className="fw-semibold th-22">Reviewed by People</div>
             <div className="fw-bold th-34">Client's Testimonials</div>
             <div className="text-secondary mt-3 mx-lg-5">
@@ -22,6 +24,30 @@ const Testimonials = () => {
             </div>
           </div>
           <div className="col-lg-2"></div>
+        </div>
+
+        <div className="row my-5 mx-lg-5 mx-2">
+          <div className="col-lg-6">
+            <TestimonialCard
+              header='"We rented a car from this website and had an amazing
+                experience! The booking was easy and the rental rates were very
+                affordable. "'
+              image={Harry}
+              title="Parry Hotter"
+              subTitle="Belgrade"
+            />
+          </div>
+
+          <div className="col-lg-6 mt-lg-0 mt-5">
+            <TestimonialCard
+              header='"The car was in great condition and made our trip even better
+              and successfull. Highly recommend to rent any car from this car
+              rental website!"'
+              image={Ron}
+              title="Ron Rizzly"
+              subTitle="Novi Sad"
+            />
+          </div>
         </div>
       </div>
 
